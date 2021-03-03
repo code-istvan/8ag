6604ba7642e2eaa16e1345a8ff1344884ccede13
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-03-03 16:02:09 +0100

    facebook ot

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar8 from "./components/Navbar8";
 import Footer from "./components/Footer";
 import Mainimage from "./components/Image";
+import MainimageSmall from "./pics/landing_img_small.jpg";
 //Router
 import { Switch, Route } from "react-router-dom";
 // Import Pages
@@ -34,6 +35,7 @@ const App = () => {
           name="description"
           content="Nyolcágú Jóga Alapítvány honlapja Az Alapítvány célja, hogy a jóga elméletét és gyakorlatát minél szélesebb körben és folyamatosan népszerűsítse Magyarországon. Az Indiából eredeztethető jóga már évezredek óta segíti az embereket a testi és lelki egészség elérésében és megőrzésében. "
         />
+        {/* <meta property="og:image" content={MainimageSmall} /> */}
       </Helmet>
       <Navbar8 />
       <Switch>
