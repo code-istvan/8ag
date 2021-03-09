91474242a09c37ff413fc1af867be3c516684e6d
Kashyap Kaki <kashyap.kaki2@boeing.com>
2021-03-09 17:20:13 +0530

    Modified react-dom render for open graph meta tags

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { hydrate, render } from "react-dom";
 
 // class App extends React.Component {
 //   state = { checked: false }
 
-ReactDOM.render(
-  <BrowserRouter>
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+  </BrowserRouter>, rootElement);
+} else {
+  render(<BrowserRouter>
+    <App />
+  </BrowserRouter>, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
