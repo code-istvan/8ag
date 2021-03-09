212cbb5922576a11cd41f04d7ae753bf39d8e1ee
Kashyap Kaki <kashyap.kaki2@boeing.com>
2021-03-09 18:35:34 +0530

    Base URL changed

--- src/pages/Fooldal.js
+++ src/pages/Fooldal.js
@@ -19,7 +19,7 @@ const Fooldal = () => {
           property="og:description"
           content="Nyolcágú Jóga Alapítvány honlapja Az Alapítvány célja, hogy a jóga elméletét és gyakorlatát minél szélesebb körben és folyamatosan népszerűsítse Magyarországon. Az Indiából eredeztethető jóga már évezredek óta segíti az embereket a testi és lelki egészség elérésében és megőrzésében. "
         />
-        <meta property="og:image" content={window.location.origin + img} />
+        <meta property="og:image" content={"https://nyolcag.hu" + img} />
         <meta property="og:url" content={window.location.origin} />
         <meta property="og:type" content="object" />
         <meta property="fb:app_id" content="1076431962839514" />
