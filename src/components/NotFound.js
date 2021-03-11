aae6f231eecf56112242daa9458292304739d9a0
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-03-11 09:32:10 +0100

    404 changes

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -6,27 +6,34 @@ import Link from "./StyledComponents/Link";
 import MetaTags from "../components/MetaTags";
 import seoImage from "../pics/landing_img_small.jpg";
 import Mainimage from "../components/Image";
+import Button from "react-bootstrap/Button";
+import "./notfound.css";
 
 const NotFound = () => {
-    return (
-        <React.Fragment>
-        <MetaTags
-          title="404-es oldal - Nyolcág"
-          img={seoImage}
-          description="Nem találjuk ezt az oldalt"
-        />
-        <Mainimage />
-<Container className="not-found">
-<Row>
-    <Col>
-    <h2>Bocs...</h2>
-    <p>Ezt az oldalt nem találjuk</p>
-    <Link href="/">Vissza a főoldalra</Link>
-    </Col>
-</Row>
-</Container>
-</React.Fragment>
-    ); 
-}; 
+  return (
+    <React.Fragment>
+      <MetaTags
+        title="404-es oldal - Nyolcág"
+        img={seoImage}
+        description="Nem találjuk ezt az oldalt"
+      />
+      <Mainimage />
+      <Container className="not-found maincontainer">
+        <Row>
+          <Col>
+            <h2>404-es oldal...bocs...ezt az oldalt nem találjuk!</h2>
+            <p>
+              Vagy kitöröltük az oldal tartalmát, aminek a linkjével
+              rendelkeztél. Vagy helytelen linket gépeltél be. Sammi gond, jó
+              helyen vagy! Menj a főoldalra és kezdd újra!
+            </p>
+            <Button href="/">Vissza a főoldalra</Button>
+            <br />
+          </Col>
+        </Row>
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default NotFound;
